53cf5bc2d9d36aad8eaa436184439fca396d2b98
Ankit Wadhwa <ankitwadhwa1402.com>
2022-08-07 01:03:10 +0530

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
-import memories from './images/memories.png';
+import pic from './images/pic.png';
 
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
@@ -37,8 +37,8 @@ const App = () => {
             <Link href="#" underline="none">
                Query 
             </Link></Typography>
-            <Typography className={classes.heading} variant="h6" align="left">Ankit diary</Typography>
-            <img className={classes.image} src={memories} alt="icon" height="60" />
+            <Typography className={classes.heading} variant="h6" align="left">Ankit Wadhwa</Typography>
+            <img className={classes.image} src={pic} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
